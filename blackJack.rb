1a3a549ab780866a2476030a483454b922f2263c
Alejandro Villegas <neopilatos@gmail.com>
2016-07-18 18:38:59 -0500

    Avance BlackJack

--- blackJack.rb
+++ blackJack.rb
@@ -1,1 +1,18 @@
-blackJack.rb
+
+
+class Card
+	attr_reader :suit, :value
+
+	def initialize(suit, value)
+		@suit = suit
+		@value = value
+	end
+
+	def value
+		return 10 if ["J", "Q", "K"].include?(@value)
+		return 11 if @value == "A"
+		return @value
+	end
+end
+
+c1 = Card.new(:hearts,9)
