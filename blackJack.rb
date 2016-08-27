e2bd7a086dcccd4cc50e393028f0c972231518f2
Alejandro Villegas <neopilatos@gmail.com>
2016-08-27 17:48:26 -0500

    Ruby Backjack

--- blackJack.rb
+++ blackJack.rb
@@ -15,5 +15,52 @@ class Card
 	end
 end
 
-c1 = Card.new(:hearts,9)
-c2 = 
+class Deck
+	attr_reader :cards
+
+	def initialize
+		@cards= []
+		build_cards
+	end	
+
+	def take!
+		@cards.shift
+	end
+
+	private
+		def build_cards
+			[:clubs, :diamonds, :spades, :hearts].each do |suit|
+			(2..10).each do |number|
+				@Cards << Card.new(suit, number)
+			end
+			["J","Q","K","A"].each do |face|
+				@Cards << Card.new(suit, face)	   	
+			end
+		end
+		@Cards.shuffle!
+	end	
+end
+
+class Hand
+	attr_reader :cards
+
+	def initialize(deck)
+		@deck = deck
+		@cards = []
+	end
+
+	def hit!
+		@cards << @deck.take!
+	end
+
+	def value
+		val = 0
+		@cards.each do |card|
+			val += card.value
+		end
+	end
+end
+
+deck = Deck.new
+hand = Hand.new(deck)
+
